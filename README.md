10d7b4eb3ccec82db5783e4c64df0ef621054fce
Reid Carlberg <reid.carlberg@gmail.com>
2015-03-04 12:28:48 +0000

    readme, new host

---
+++ README.md
@@ -0,0 +1,10 @@
+Simple Data Recorder
+====================
+
+See also: [Naive Data Host](https://github.com/ReidCarlberg/naive-iot-host)
+
+Polls Arduinos for data using the I2C protocol.  Sends data to a cloud host.
+
+Designed to run on a Raspberry Pi.
+
+[@ReidCarlberg](https://twitter.com/ReidCarlberg)
--- dataRead.js
+++ dataRead.js
@@ -4,12 +4,12 @@ var i2c = require('i2c'),
 
 var address = 0x18;
 
-var sampleSize = 30; // number of samples to take before sending to host
+var sampleSize = 10; // number of samples to take before sending to host
 var currentSampleCount = 0;
-var samplePeriod = 10000; // milliseconds between polling for samples
+var samplePeriod = 30000; // milliseconds between polling for samples
 
 //var url = 'http://192.168.1.10:5000/report';
-var url = 'https://nameless-brushlands-6436.herokuapp.com/report';
+var url = 'https://naive-iot-host.herokuapp.com/report';
 
 var device1 = new i2c(address, {device: '/dev/i2c-1'});
 device1.setAddress(0x4);
@@ -32,14 +32,16 @@ function handleRead() {
 			res = res.toString('ascii');
 			results.record(res);
 		});
-		console.log(results.data);
 	}
+
+	console.log(results.data);
 
 	if (currentSampleCount == sampleSize) {
 		//send to host
 		request( { method: 'POST', uri: url, 
 			json: true, body: results.data } , 
-			function() {
+			function(error, response, body) {
+				if (error) { console.log ( error ); }
 				console.log('Reported');
 				results.data = {};
 				currentSampleCount = 0;
